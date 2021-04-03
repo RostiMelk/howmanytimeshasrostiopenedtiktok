9111e64e853ee145150c9f3ee4a5b69b21469b9c
RostiMelk <hello@rosti.no>
2021-04-03 17:29:12 +0200

    updated tailwind purge settings

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,17 @@
 module.exports = {
-  purge: [
-    './src/**/*.js',
-  ],
-  darkMode: 'media', // or 'media' or 'class'
+  purge: {
+    enabled: true,
+    content: [
+      './src/**/*.js',
+    ],
+    options: {
+      safelist: ['bg-green-100','bg-red-100','bg-yellow-100','text-green-600','text-red-600','text-yellow-600'],
+      blocklist: [/^debug-/],
+      keyframes: true,
+      fontFace: true,
+    },
+  },
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
